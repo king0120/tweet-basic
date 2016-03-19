3604e7c33276c06bca1828d8476a189a0c6cc1c2
Anthony Sims Jr <anthony.sims.226@gmail.com>
2016-03-19 09:44:45 -0400

    add twitter connection, bootstrap and jquery

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var morgan  = require('morgan');
 var express = require('express');
 var app     = express();
-var Twit    = require('twit');
 var server  = require('http').createServer(app);
 var port    = process.env.PORT || 3000;
 var router  = express.Router();
@@ -20,6 +19,31 @@ app.use(morgan('dev'));
 //     res.render('index');
 // });
 
+// INTEGRATE SOCKET
+var io = require('socket.io')(server);
+var Twit    = require('twit');
+
+var twitter = new Twit({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token: process.env.TWITTER_ACCESS_TOKEN,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
+var stream = twitter.stream('statuses/filter', { track: 'javascript'});
+
+io.on('connect', function(socket) {
+  stream.on('tweet', function (tweet) {
+  var data = {};
+    data.name = tweet.user.name;
+    data.screen_name = tweet.user.screen_name;
+    data.text = tweet.text;
+    data.user_profile_image = tweet.user.profile_image_url;
+    socket.emit('tweets', data);
+  });
+});
+
+// ROUTES
 router.get('/', function(req, res) {
   res.render('index', { header: 'Twitter Search'});
 });
@@ -36,5 +60,3 @@ app.use('/', router);
 server.listen(port);
 
 console.log('Server started on ' + port);
-
-var io = require('socket.io')(server);
